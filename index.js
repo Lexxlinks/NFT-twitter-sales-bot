79059ddbb6b526f1f801a8faeb53e566eac99c16
lexxlinks <savvysovereign@protonmail.com>
2023-03-06 22:37:44 -0700

    index again

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { TwitterApi } from "twitter-api-v2";
 import dotenv from "dotenv";
 import path from "path";
 
+
+
+
 dotenv.config({ path: path.join(new URL(import.meta.url).pathname, ".env") });
 
 const API_KEY = process.env.API_KEY;
@@ -10,12 +13,13 @@ const API_SECRET = process.env.API_SECRET;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const ACCESS_SECRET = process.env.ACCESS_SECRET;
 
-const NFT_CONTRACT_ADDRESS = "";
+const NFT_CONTRACT_ADDRESS = "0xd563272eea17F8CE929171c2bA62b1c7FB4756aE";
 const NFT_TOKEN_ID = 1;
 const NETWORK = "mainnet";
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
 
-const provider = new ethers.providers.InfuraProvider(NETWORK, INFURA_PROJECT_ID);
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"implementation","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"stateMutability":"payable","type":"receive"}];
+const provider = new ethers.InfuraProvider(NETWORK, INFURA_PROJECT_ID);
 const contract = new ethers.Contract(NFT_CONTRACT_ADDRESS, abi, provider);
 
 const twitterClient = new TwitterApi({
